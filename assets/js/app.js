65d70b69ff9d791d8b4bd03917d9dfcbced6eaf3
Kenny <kpstanleyjr@gmail.com>
2020-06-11 13:50:51 -0500

    Changed smooth scrolling from CSS to pure JS.

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,8 +1,38 @@
 const menu = document.querySelector('#mobile-menu');
 const menuLinks = document.querySelector('.nav-menu');
+const arrowBtn = document.querySelector('.arrowbtn')
 
 menu.addEventListener('click', function() {
     menu.classList.toggle('is-active');
     menuLinks.classList.toggle('active');
 });
 
+
+function smoothScroll(target, duration) {
+    var target = document.querySelector(target);
+    var targetPosition = target.getBoundingClientRect().top;
+    var startPosition = window.pageYOffset;
+    var distance = targetPosition - startPosition;
+    var startTime = null;
+
+    function animation(currentTime) {
+        if(startTime === null) startTime = currentTime;
+        var timeElapsed = currentTime - startTime;
+        var run = ease(timeElapsed, startPosition, distance, duration);
+        window.scrollTo(0, run);
+        if (timeElapsed < duration) requestAnimationFrame(animation);
+    }
+
+    function ease(t, b, c, d) {
+        t /= d / 2;
+        if (t < 1) return c / 2 * t * t + b;
+        t--;
+        return -c / 2 * (t * (t - 2) - 1) + b;
+    }
+
+    requestAnimationFrame(animation);
+}
+
+arrowBtn.addEventListener('click', function(){
+    smoothScroll(this.getAttribute('href'), 1000)
+});
