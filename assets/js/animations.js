69b2cdae87284d94d4c926aed44960595b23014f
kpstanleyjr <kpstanleyjr@gmail.com>
2020-06-11 19:14:35 -0500

    Added some animations.

---
+++ assets/js/animations.js
@@ -0,0 +1,17 @@
+
+
+gsap.from('.main-title', {opacity: 0, duration: 1, y: -400, ease: "power2.inOut"});
+// gsap.from('.main-subtitle', {opacity: 0, duration: 1, ease: "power2.inOut"});
+
+gsap.from('.pink.line', {opacity: 0, duration: 4, y: -1500, ease: "power2.inOut"});
+gsap.from('.blue.line', {opacity: 0, duration: 4, x: 1500, ease: "power2.inOut"});
+gsap.from('.arrow-btn', {opacity: 0, duration: 3, x: -1500, ease: "power2.inOut"});
+
+const t1 = new TimelineMax();
+
+t1.fromTo(".anim-typewriter", 5, { 
+    width: "0"
+}, { 
+    width: "800px",
+    ease:  SteppedEase.config(18) /* Number of characters in string. */
+}, 0)
