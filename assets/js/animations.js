fd33b07d51cc3f15bf8736a24f3b79f57574af7a
Kenny <kpstanleyjr@gmail.com>
2020-06-14 11:35:22 -0500

    sped up arrow button animation

--- assets/js/animations.js
+++ assets/js/animations.js
@@ -1,11 +1,11 @@
 
 
 gsap.from('.main-title', {opacity: 0, duration: 1, y: -400, ease: "power2.inOut"});
-gsap.from('.main-subtitle', {opacity: 0, duration: 2, delay: 0.5, ease: "power2.inOut"});
+gsap.from('.main-subtitle', {opacity: 0, duration: 2, delay: 0.6, ease: "power2.inOut"});
 
 gsap.from('.pink.line', {opacity: 0, duration: 4, y: -1000, ease: "power2.inOut"});
 gsap.from('.blue.line', {opacity: 0, duration: 3, x: 1000, ease: "power2.inOut"});
-gsap.from('.arrow-btn', {opacity: 0, duration: 3, delay: 1.2, y: 500, ease: "power2.inOut"});
+gsap.from('.arrow-btn', {opacity: 0, duration: 2, delay: 0.5, y: 200, ease: "power2.inOut"});
 
 // const t1 = new TimelineMax();
 
